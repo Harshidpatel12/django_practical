6437349e8d61ad5ec0da00f259e83689dd9cb952
Harshid Patel <harshidp12@gmail.com>
2025-04-09 22:53:11 +0530

    Initt 3

--- quora/validators.py
+++
@@ -1,5 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_password_length(value):
-    if len(value) < 5:
-        raise ValidationError("Password must be at least 5 characters long.") 
